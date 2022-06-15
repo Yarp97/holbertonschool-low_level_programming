96c6a8a6e6198a4218e828e77cfaae84e8e7747c
Yarp97 <5212@holbertonschool.com>
2022-06-14 19:13:56 -0500

    punto 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-printf("Size of char: %zu byte\n", sizeof(char));
-printf("Size of int: %zu bytes\n", sizeof(int));
-printf("Size of long int: %zu bytes\n", sizeof(long));
-printf("Size of long long int: %zu bytes\n", sizeof(long long));
-printf("Size of float: %zu bytes\n", sizeof(float));
+printf("Size of a char: %zu byte\n", sizeof(char));
+printf("Size of an int: %zu bytes\n", sizeof(int));
+printf("Size of a long int: %zu bytes\n", sizeof(long));
+printf("Size of a long long int: %zu bytes\n", sizeof(long long));
+printf("Size of a float: %zu bytes\n", sizeof(float));
 return (0);
 }
