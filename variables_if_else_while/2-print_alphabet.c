57e3b1014a5b981003ad5e72bc89dd8267255826
Yarp97 <5212@holbertonschool.com>
2022-06-15 23:52:37 -0500

    punto 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main-prints the alphabet
+*Return: (0)
+*/
+
+
+int main(void)
+{
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+
+return (0);
+}
