ff31944f1b8663a6bf1079afaaa4f567432239f9
Yarp97 <5212@holbertonschool.com>
2022-06-16 00:21:35 -0500

    punto 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase whitout "q" and "e"
+ *
+ *Return: (0)
+ */
+
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+if (ch != 'e' && ch != 'q')
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
