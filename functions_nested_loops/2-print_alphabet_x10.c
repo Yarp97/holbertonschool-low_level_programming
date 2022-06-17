243ebb20a44d4045251b7279908e87f6cf951ef6
Yarp97 <5212@holbertonschool.com>
2022-06-17 18:47:02 -0500

    Prints alphabetx10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * void print_alphabet_x10(void) - prints alphabet
+ * Return: (void)
+ */
+
+void print_alphabet_x10(void)
+
+{
+int a, b;
+for ( b = 0; b < 10; b++ )
+{
+for ( a = 97; a <= 122; a++ )
+{
+_putchar(a);
+
+}
+_putchar(10);
+}
+}
