f7984e4e6b220fb3414db4d375d0a5a4719002c8
Yarp97 <5212@holbertonschool.com>
2022-06-17 18:49:43 -0500

    Prints alphabetx10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 
 {
 int a, b;
-for ( b = 0; b < 10; b++ )
+for (b = 0; b < 10; b++)
 {
-for ( a = 97; a <= 122; a++ )
+for (a = 97; a <= 122; a++)
 {
 _putchar(a);
 
