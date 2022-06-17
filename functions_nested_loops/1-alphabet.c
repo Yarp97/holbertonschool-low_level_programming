a5fbbbe114db2cf425030c2b5c6377a29ca518a1
Yarp97 <5212@holbertonschool.com>
2022-06-17 13:47:10 -0500

    prints alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+char a;
+for (a = 97; a <= 122; a++)
+{
+_putchar(a);
+}
+_putchar(10);
+}
