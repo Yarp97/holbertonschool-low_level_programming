5423951639eccc0b7741773514f5c130acd8a6fe
Yarp97 <5212@holbertonschool.com>
2022-06-21 10:54:19 -0500

    Prints numbers 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_numbers - Prints numbers 0 to 9
+ *
+ *Return: number
+ */
+
+void print_numbers(void)
+  
+{
+char c;
+for (c = 48; c <= 57; c++)
+{
+_putchar(c);
+}
+_putchar(10);
+
+}
