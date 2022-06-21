e71f3b16822a8b6bf2672ce6c47c411ac5326b2a
Yarp97 <5212@holbertonschool.com>
2022-06-21 13:20:20 -0500

    print number 0 to 9, but do not print 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_numbers - Prints numbers 0 to 9
+ *
+ *Return: number
+ */
+
+void print_most_numbers(void)
+{
+char c;
+for (c = 48; c <= 57; c++)
+{
+if (c != 50 && c != 52)
+_putchar(c);
+}
+_putchar(10);
+
+}
