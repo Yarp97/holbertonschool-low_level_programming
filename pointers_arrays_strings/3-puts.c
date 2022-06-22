70cb1f948775b39c1334ba1ff69f6f60cb1cecd8
Yarp97 <5212@holbertonschool.com>
2022-06-22 17:31:03 -0500

    prints a string, followed by a new line

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *_puts - prints a string, followed by a new line
+ *@str: string
+ *
+ */
+
+void _puts(char *str)
+
+{
+puts(str);
+
+}
