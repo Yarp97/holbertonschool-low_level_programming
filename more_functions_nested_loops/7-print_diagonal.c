6fbffad0136650a36abff6fbf6ebe5ae97db6576
Yarp97 <5212@holbertonschool.com>
2022-06-21 21:20:56 -0500

    prints diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include"main.h"
+
+/**
+ * print_diagonal - prints diagonal
+ * @n: number
+ * return: number
+ */
+
+void print_diagonal(int n)
+{
+int a;
+int b;
+
+if (n > 0)
+{
+for (a = 0; a < n; a++)
+{
+for (b = 0; b < a; b++)
+{
+_putchar(' ');
+}
+_putchar(92);
+_putchar(10);
+}
+}
+else
+{
+_putchar(10);
+}
