82af1136927444561b7107b691f3c97a89dc60f4
Yarp97 <5212@holbertonschool.com>
2022-06-28 17:23:58 -0500

    concatenates two string

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ **_strcat - cancatenates two string
+ *@dest: string destino
+ *@src: string world
+ *Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+
+int posicion1, posicion2;
+
+for (posicion1 = '0'; dest[posicion1] != 0; posicion1++)
+{
+for (posicion2 = '0'; src[posicion2] != 0; posicion2++)
+{
+dest[posicion1 + posicion2] = src[posicion1];
+dest[posicion1 + posicion2] = '\0';
+}
+}
+
+return (dest);
+}
