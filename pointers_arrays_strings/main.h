8f4f6f8e0ccd7085238bf964f91705e17ce45373
Yarp97 <5212@holbertonschool.com>
2022-06-28 17:22:21 -0500

    main.h include functions prototypes

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -14,7 +14,7 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-//C - More pointers, arrays and strings//
+/*C - More pointers, arrays and string*/
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
